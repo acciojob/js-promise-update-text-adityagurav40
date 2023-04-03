c23fc613721863ffcf3da8b17f31364e6a475a75
adityagurav40 <113020833+adityagurav40@users.noreply.github.com>
2023-04-03 15:49:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-//your JS code here. If required.
 function sayHello() {
-	return new promise((resolve) =>{
-		setTimeout(() =>{
-			resolve("Hello,world!");
-		},1000);
-	});
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
 }
-const output = document.getElementById("output");
-sayHello().then((result) =>{
-	output.textContent = result;
+const outputElement = document.getElementById("output");
+
+sayHello().then((message) => {
+  outputElement.textContent = message;
 });
